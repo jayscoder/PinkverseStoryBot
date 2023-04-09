94483bc951be33bd6c6a737f21a84d32080fbd59
王童 <785271992@qq.com>
2023-04-09 16:24:23 +0800

    增加了GPT字符数限制

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ bot = discord.Client(
 )  # 指定了客户端对象需要接收所有的事件，包括一些敏感信息，例如用户列表、权限等等。需要注意的是，Discord在2022年4月7日之后，所有新创建的应用必须填写和审核 Intents 后才能使用它们。
 
 # 全局配置
-MAX_GPT_TOKENS = 2000  # GPT最多上传2000个字符
+MAX_GPT_TOKENS = 10000  # GPT最多上传10000个字符
 MAX_DISCORD_TOKENS = 2000  # discord一次最多可以发送2000个字符的消息
 
 HELP_CONTENT = '''!help: 获取当前的指令手册
