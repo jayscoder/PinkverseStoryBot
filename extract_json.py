f4343b6c1d1ad78f5950514b2c5bb00801752222
王童 <785271992@qq.com>
2023-04-25 18:22:17 +0800

    add cooper

--- extract_json.py
+++ extract_json.py
@@ -3,6 +3,7 @@ import json
 from typing import Union, Optional
 import re
 
+
 def replace_single_quotes(match):
     match_str = match.group()
     if match_str.startswith("'") and match_str.endswith("'") and len(match_str) > 1:
@@ -10,18 +11,11 @@ def replace_single_quotes(match):
     else:
         return match_str
 
-sample_text = '''dadada{"name": 'hello'}下面是生成的内容
-{"action": "严肃",
-"button_sequence" : ["钱", '一起', '干活'],
-"voice":"努力工作，加班挣更多的钱！",
-"thought_emoji":"💰💰",
-"thought_text":"想要更多的钱"}
-（提示：请注意尊重自己和他人的合法权益及规则，不要做出违法违规的行为）'''
 
 # 使用正则表达式匹配JSON字符串
 
 def extract_json(content: str) -> list:
-    json_matches = re.findall(r'\{[\s\S]*?\}', sample_text)
+    json_matches = re.findall(r'\{[\s\S]*?\}', content)
     results = []
     for json_str in json_matches:
         print(json_str)
@@ -32,4 +26,12 @@ def extract_json(content: str) -> list:
 
 
 if __name__ == '__main__':
+    sample_text = '''dadada{"name": 'hello'}下面是生成的内容
+    {"action": "严肃",
+    "button_sequence" : ["钱", '一起', '干活'],
+    "voice":"努力工作，加班挣更多的钱！",
+    "thought_emoji":"💰💰",
+    "thought_text":"想要更多的钱"}
+    （提示：请注意尊重自己和他人的合法权益及规则，不要做出违法违规的行为）'''
+
     print(extract_json(sample_text))
